85cbe8d6fded6111114172c90528f2c9b14d4e4d
Samuel <50973822+slastrina@users.noreply.github.com>
2021-07-07 19:23:32 +1000

    modified paged_query

--- django_ldap/utils.py
+++ django_ldap/utils.py
@@ -1,31 +1,15 @@
-from collections import defaultdict
-
-from ldap3 import ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES, SUBTREE
+from ldap3 import SUBTREE
 
 from django_ldap.ldap import ldap_connection
 
 
-def get_paged_result(base, filter, attr, key, raw=False):
-    results = defaultdict()
-
-    with ldap_connection() as connection:
-
-        entry_list = connection.extend.standard.paged_search(
+def paged_query(base, filter, attr, generator=False):
+    with ldap_connection() as conn:
+        return conn.extend.standard.paged_search(
             search_base=base,
             search_filter=filter,
             search_scope=SUBTREE,
             attributes=attr,
             paged_size=500,
-            generator=False,
+            generator=generator,
         )
-
-        for entry in entry_list:
-            try:
-                name = entry["attributes"][key]
-                if attr == [ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES] or raw == True:
-                    results[name] = entry["attributes"]
-                else:
-                    results[name] = {a: entry["attributes"][a] for a in attr}
-            except:
-                pass
-    return results
